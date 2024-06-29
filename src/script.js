ab135e16a333936858d96ef5e6139a3c1b8942b1
Jean SAUVAGE <jsauvage@e3r5p19.clusters.42paris.fr>
2024-06-29 15:04:37 +0200

    circle pattern

---
+++ src/script.js
@@ -0,0 +1,78 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import circleVertexShader from './shaders/circle/vertex.glsl'
+import circleFragmentShader from './shaders/circle/fragment.glsl'
+
+const canvas = document.querySelector('canvas.webgl')
+const scene = new THREE.Scene()
+
+/**
+ * Test mesh
+ */
+// Geometry
+const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
+
+// Material
+const material = new THREE.ShaderMaterial({
+    vertexShader: circleVertexShader,
+    fragmentShader: circleFragmentShader,
+})
+
+// Mesh
+const circle = new THREE.Mesh(geometry, material)
+scene.add(circle)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0.25, - 0.25, 1)
+scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+
+const tick = () =>
+{
+    controls.update()
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(tick)
+}
+
+tick()
